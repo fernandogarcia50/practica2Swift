e665f7fe015ac9315ae35b9b4b56a306acf85ea3
Alexis roman <elsanguinario3257@gmail.com>
2022-02-16 09:10:18 -0600

    primero

---
+++ EnvioParametros/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnvioParametros
+//
+//  Created by Mac11 on 16/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
